83d96510cb6e23283a14cc5aff3b0cf3c3b48511
Valerii_Panov <val250640@mail.ru>
2022-11-25 21:00:15 +1000

    EndCreateProgram

--- Program.cs
+++ Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Написать программу, которая из имеющегося массива строк формирует массив из строк, дли которых меньше либо равна 3 символа.
+
+(string[], int) EnterTheStrings(int size)
+{
+    string[] startArray = new string[size];
+    int counter = 0;
+
+    Console.WriteLine("Enter the strings (<end> to finish):");
+    while (counter < size)
+    {
+        startArray[counter] = Console.ReadLine();
+        if (startArray[counter] != "end") counter++;
+        else break;
+    }
+
+    return (startArray,counter);
+}
+
+string[] CheckArray(string[] startArray, int counter)
+{
+    string[] finalArray = new string[counter];
+
+    for (int i = 0, j = 0; i < counter; i++)
+        if (startArray[i].Length <= 3) finalArray[j++] = startArray[i];
+
+    return finalArray;
+}
+
+
+
+int size = 10;
+(string[] startArray, int counter) = EnterTheStrings(size);
+System.Console.WriteLine(String.Join(' ', startArray));
+
+string[] finalArray = CheckArray(startArray, counter);
+System.Console.WriteLine(String.Join(' ', finalArray));
